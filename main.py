b530f418bf573db87dd02231f0a312de5110197a
Tailor Andres Maffini López <tailor.alm@gmail.com>
2024-12-20 00:15:02 +0000

    Configure Docker

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
+load_dotenv()
+
 from src.shared.enum.folders import Folders
 from src.shared.utils.general import ensure_directory_existence
 from src.modules.telegram_bot.router import Router
 
-load_dotenv()
 
 for folder in Folders:
     ensure_directory_existence(f"{folder.value}/any.jpg")
